12e3767dfd179600e0f55ec698f680e2235b6bda
horchi <vdr@jwendel.de>
2023-01-12 09:49:20 +0100

    minor changes

---
+++ scripts/trigger-tft-reload.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ -f /storage/.config/default/vdrtft ]]; then
+   source /storage/.config/default/vdrtft
+fi
+
+if [[ -n ${TFT_HOST} ]]; then
+   /usr/bin/ssh pi@${TFT_HOST} /home/pi/bin/toggle.sh restart
+fi
--- scripts/trigger-tft-shutdown.sh
+++ scripts/trigger-tft-shutdown.sh
@@ -4,11 +4,15 @@ if [[ -f /storage/.config/default/vdrtft ]]; then
    source /storage/.config/default/vdrtft
 fi
 
-if [[ "${TFT_SHUTDOWN}" == "yes" ]]; then
-   ssh root@${TFT_HOST} shutdown -h now
-elif [[ "${TFT_SHUTDOWN}" == "on-vdrosd" ]]; then
-   ssh pi@${TFT_HOST} systemctl --user is-active --quiet status vdrosd
-   if [ ${?} == "0" ]; then
+if [[ -n ${TFT_HOST} ]]; then
+
+   if [[ "${TFT_SHUTDOWN}" == "yes" ]]; then
       ssh root@${TFT_HOST} shutdown -h now
+   elif [[ "${TFT_SHUTDOWN}" == "on-vdrosd" ]]; then
+      ssh pi@${TFT_HOST} systemctl --user is-active --quiet status vdrosd
+      if [ ${?} == "0" ]; then
+         ssh root@${TFT_HOST} shutdown -h now
+      fi
    fi
+
 fi
